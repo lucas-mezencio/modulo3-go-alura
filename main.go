bb1314ce47614b521ada0ff7b10beb5ddd3478a0
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-01 15:21:00 -0300

    Webapp: struct de produtos

--- main.go
+++ main.go
@@ -5,15 +5,37 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8000", nil)
+	_ = http.ListenAndServe(":8000", nil)
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{
+			Nome:       "camiseta",
+			Descricao:  "Azul escuro",
+			Preco:      39,
+			Quantidade: 10,
+		},
+		{
+			"Tenis", "Comfortável", 200, 5,
+		},
+		{
+			"Fone", "pure bass", 150, 10,
+		},
+	}
+
+	_ = templates.ExecuteTemplate(w, "Index", produtos)
 }
