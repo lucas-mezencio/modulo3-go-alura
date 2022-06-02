7038f872cc82e11c474cdcacf6c26ef15b966952
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-02 11:10:16 -0300

    Webapp: inserindo dados no banco

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 	"web-crud/models"
 )
 
@@ -11,4 +13,30 @@ var templates = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	produtos := models.BuscaTodosOsProdutos()
 	_ = templates.ExecuteTemplate(w, "Index", produtos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	_ = templates.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversao do preço:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversao da quantidade:", err)
+		}
+
+		models.CriarProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
 }
--- models/produto.go
+++ models/produto.go
@@ -41,3 +41,15 @@ func BuscaTodosOsProdutos() []Produto {
 	defer dababase.Close()
 	return produtos
 }
+
+func CriarProduto(nome, descricao string, preco float64, quantidade int) {
+	database := db.ConnectWithDB()
+
+	scriptInsercao, err :=
+		database.Prepare("insert into produtos(nome, descricao, preco, quantidade) values($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+	scriptInsercao.Exec(nome, descricao, preco, quantidade)
+	defer database.Close()
+}
