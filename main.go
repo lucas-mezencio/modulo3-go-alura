08d15bf7b5e5fb1510d3b8d7709f0529b57f0b96
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-02 10:00:49 -0300

    Webapp: conexao com banco de dados - create

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -22,20 +26,44 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{
-			Nome:       "camiseta",
-			Descricao:  "Azul escuro",
-			Preco:      39,
-			Quantidade: 10,
-		},
-		{
-			"Tenis", "Comfortável", 200, 5,
-		},
-		{
-			"Fone", "pure bass", 150, 10,
-		},
+	db := connectWithDB()
+
+	selectTodosProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectTodosProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectTodosProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	_ = templates.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
+}
+
+func connectWithDB() *sql.DB {
+	connection := "user=postgres dbname=postgres password=12345678 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
